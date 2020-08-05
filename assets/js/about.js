415d9767455bb5373b7b70556cce383dc4f568bb
fullender <mdenyabut@yahoo.com>
2020-08-05 19:52:51 +0800

    initial commit

---
+++ assets/js/about.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+ // Set a variable for our button element.
+const scrollToTopButton = document.getElementById('js-top');
+
+window.onscroll = function() {checkScroll()};
+function checkScroll(){
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        scrollToTopButton.style.display = "block";
+    } else {
+        scrollToTopButton.style.display = "none";
+    }
+}
+
+// Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
+const scrollFunc = () => {
+  // Get the current scroll value
+  let y = window.scrollY;
+  
+  // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+  if (y > 0) {
+    scrollToTopButton.className = "top-link show";
+  } else {
+    scrollToTopButton.className = "top-link hide";
+  }
+};
+
+window.addEventListener("scroll", scrollFunc);
+});
---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+/*$(document).ready(function() {
+
+  $(window).scroll(function() {
+    
+    if($(this).scrollTop() > 90) { 
+        $('.navbar').addClass('solid');
+         $("ul,li,a").addClass('text-light');
+         $("ul,li,a").removeClass('text-dark');
+    } else {
+        $('.navbar').removeClass('solid');
+        $("ul,li,a").addClass('text-dark');
+        $("ul,li,a").removeClass('text-light');
+
+
+        
+    }
+  });
+});*/
+	// window.load(function() {
+	// 	$(".se-pre-con").fadeOut("slow");;
+	// });
